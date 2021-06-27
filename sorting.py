12072e107b3c3ce1eff7fc2c8b2ad3f482a50dc4
Kerem Yagan <keremyagann@gmail.com>
2021-06-27 22:11:48 +0300

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+dosya=input("Lütfen Dosya Konumunu Giriniz:")
+ilk_kac_token=int(input("İlk Kaç Token Yazdırılsın:"))
+icerik=open(dosya,"r").readlines()
+siralama=Counter(icerik).most_common()
+a=1
+for i in siralama[:ilk_kac_token]:
+    print(a,"-",i[0].rstrip("\n"),i[1])
+    a=a+1
